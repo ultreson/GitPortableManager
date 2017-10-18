746d257c5d45c389fd2c08d08d24bca97ce28ae2
ultreson <alexitessier@hotmail.com>
2017-10-18 10:27:19 -0400

    moved data files to folder

--- FirstTimeSetup.bat
+++ FirstTimeSetup.bat
@@ -1,20 +1,21 @@
 Setlocal EnableDelayedExpansion
 @echo off
+if not exist data mkdir data
 SET /P email=enter your git email : 
-echo %email%> email.txt
+echo %email%> data\email.txt
 SET /P username=enter your git username : 
-echo %username%> username.txt
+echo %username%> data\username.txt
 echo enter where you want the projects to be cloned
 SET /P location=%USERPROFILE%\
 if not exist %USERPROFILE%\%location% mkdir %USERPROFILE%\%location%
-echo %location%> location.txt
+echo %location%> data\location.txt
 SET /P nbProjects=enter the number of projects to clone : 
 SET /P firstproject=enter the name of the first project : 
-echo %firstproject%> projects.txt
+echo %firstproject%> data\projects.txt
 IF %nbProjects% GTR 1 (
 	SET nbProjects = %nbProjects% 
 	FOR /L %%A IN (2,1,%nbProjects%) DO (
 		SET /P project=enter the name of  project %%A : 
-		echo !project!>> projects.txt
+		echo !project!>> data\projects.txt
 	)
 )
--- gitSetup.bat
+++ gitSetup.bat
@@ -1,14 +1,14 @@
-::git clones 
+::git clones projects given with FirstTimeSetup.bat
 ::using git user email in "email.txt"
 ::and git username in "name.txt"
 ::and git project names in "projects.txt"
 ::cloning in folder given in location.txt
-
-set /p email=< email.txt
-set /p location=< location.txt
+@echo off
+set /p email=< data\email.txt
+set /p location=< data\location.txt
+set /p username=< data\username.txt
 start "" /d "%USERPROFILE%\%location%\" /w "%ProgramFiles%\Git\git-bash.exe" git config --global user.email '%email%'
-set /p username=< username.txt
-for /F "tokens=*" %%P in (projects.txt) do (
+for /F "tokens=*" %%P in (data\projects.txt) do (
 	cd /D %USERPROFILE%\%location%\
 	start /w "%ProgramFiles%\Git\git-bash.exe" git clone https://github.com/%username%/%%P.git
 	start "%%A" /D "%USERPROFILE%\%location%\%%P" "%ProgramFiles%\Git\git-bash.exe"
