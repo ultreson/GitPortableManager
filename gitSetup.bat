cb0d0f6c21f0fe568b173fc9cbabaf5bd433018b
Tessier Alexi <alexitessier@hotmail.com>
2017-10-23 17:31:26 -0400

    updated email assignation

--- gitSetup.bat
+++ gitSetup.bat
@@ -7,9 +7,9 @@
 set /p email=< data\email.txt
 set /p location=< data\location.txt
 set /p username=< data\username.txt
-start "" /d "%USERPROFILE%\%location%\" /w "%ProgramFiles%\Git\git-bash.exe" git config --global user.email '%email%'
+start /w "%ProgramFiles%\Git\git-bash.exe" git config --global user.email "%email%"
 for /F "tokens=*" %%P in (data\projects.txt) do (
 	cd /D %USERPROFILE%\%location%\
 	start /w "%ProgramFiles%\Git\git-bash.exe" git clone https://github.com/%username%/%%P.git
-	start "%%A" /D "%USERPROFILE%\%location%\%%P" "%ProgramFiles%\Git\git-bash.exe"
+	start "" /D "%USERPROFILE%\%location%\%%P" "%ProgramFiles%\Git\git-bash.exe"
 )
